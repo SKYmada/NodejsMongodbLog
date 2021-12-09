dd7fee2444b80dbb75f6d4e78d261ac5e04b9734
mjt <2779734113@qq.com>
2021-12-09 09:24:24 +0800

    install mongoose

--- NodejsMongodbLog.js
+++ NodejsMongodbLog.js
@@ -1,10 +1,9 @@
-const express = require("express");
+const express = require("express");​	
 const app = express();
-
-
-app.use('/',express.static('public'));      //将public文件夹映射到根目录下
+app.use('/',express.static('public'));      //静态文件 将public文件夹映射到根目录下
 app.get("/input",(req,res) => {
-    res.send("hello");
-    console.log(req.query);
+    res.send(req.query)
+    console.log(req.query)
 })
+
 app.listen(10317);  //最大端口号为65530
