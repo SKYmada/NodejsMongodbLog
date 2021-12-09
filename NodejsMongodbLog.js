5c6225d1589012a7dce35a0e82cd209fafb17720
mjt <2779734113@qq.com>
2021-12-09 17:22:22 +0800

    link mongodb successfully

--- NodejsMongodbLog.js
+++ NodejsMongodbLog.js
@@ -1,8 +1,22 @@
 const express = require("express");
 const app = express();
-app.use('/',express.static('public'));      //将public文件夹映射到根目录下
-app.get("/input",(req,res) => {
-    res.send("hello");
-    console.log(req.query);
-})
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test1');
+
+const schema = {
+    name:String,
+    age:Number,
+    health:String
+}
+const abc = mongoose.model('cat1s', schema);
+const kitty = new abc({name:"111"});
+kitty.save();
+
+
+// app.use('/',express.static('public'));      //将public文件夹映射到根目录下
+// app.get("/input",(req,res) => {
+//     res.send(req.query);
+//     console.log(req.query);
+// })
+
 app.listen(10317);  //最大端口号为65530
