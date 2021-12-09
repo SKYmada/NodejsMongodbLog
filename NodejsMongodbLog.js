bec57d83bbd57b6e0567b7fddb57c3ca3f0ffc32
mjt <2779734113@qq.com>
2021-12-09 17:07:02 +0800

    刷新

--- NodejsMongodbLog.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");​	
-const app = express();
-app.use('/',express.static('public'));      //静态文件 将public文件夹映射到根目录下
-app.get("/input",(req,res) => {
-    res.send(req.query)
-    console.log(req.query)
-})
-
-app.listen(10317);  //最大端口号为65530
