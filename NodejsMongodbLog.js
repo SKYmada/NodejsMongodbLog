6525e6ffbaa381be20c2dff01c4205ba9e6e04a9
mjt <2779734113@qq.com>
2021-12-13 09:25:44 +0800

    mongodb 存储成功

--- NodejsMongodbLog.js
+++ NodejsMongodbLog.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/test1');
+mongoose.connect('mongodb://172.21.2.236:27017/190110910317');
 
 const schema = {
     name:String,
@@ -9,14 +9,16 @@ const schema = {
     health:String
 }
 const abc = mongoose.model('cat1s', schema);
-const kitty = new abc({name:"111"});
-kitty.save();
+// const kitty = new abc({name:"111"});
+// kitty.save();
 
 
-// app.use('/',express.static('public'));      //将public文件夹映射到根目录下
-// app.get("/input",(req,res) => {
-//     res.send(req.query);
-//     console.log(req.query);
-// })
+app.use('/',express.static('public'));      //将public文件夹映射到根目录下
+app.get("/input",(req,res) => {
+    res.send(req.query);
+    const kitty = new abc({name: req.query.first, health: req.query.second});
+    console.log("已写入"+req.query.first+"，"+req.query.second);
+    kitty.save();
+})
 
 app.listen(10317);  //最大端口号为65530
